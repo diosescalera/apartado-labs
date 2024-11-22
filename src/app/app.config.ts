3eb3ec68272715acc4a73673852e78b1eb4d3430
diosescalera <al217108@edu.uaa.mx>
2024-11-22 08:55:14 -0600

    add login route and update app configuration with HTTP interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  
+    provideHttpClient(
+      withInterceptors([
+        (req, next) => {
+          const modifiedReq = req.clone({
+            setHeaders: {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+              'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+            }});
+          return next(modifiedReq);
+        }
+      ])
+    )
+  ]
 };
