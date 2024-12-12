2d6a1dc6e3f81c8e8eaa91ace16a816308992a15
diosescalera <al217108@edu.uaa.mx>
2024-12-12 17:46:59 -0600

    add cargar-horario route and update navbar link for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
+import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { NuevaSolicitudComponent } from "./nueva-solicitud/nueva-solicitud.component";
-import { Routes } from '@angular/router';
+import { CargarHorarioComponent } from "./cargar-horario/cargar-horario.component";
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'nueva-solicitud', component: NuevaSolicitudComponent },
+    { path: 'cargar-horario', component: CargarHorarioComponent },
 ];
