905ce7672120feda118cbb782cc9459afde97881
diosescalera <al217108@edu.uaa.mx>
2024-12-12 17:47:15 -0600

    add cargar-horario component with form, styles, and tests for scheduling functionality

---
+++ src/app/cargar-horario/cargar-horario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cargar-horario',
+  imports: [],
+  templateUrl: './cargar-horario.component.html',
+  styleUrl: './cargar-horario.component.css'
+})
+export class CargarHorarioComponent {
+
+}
