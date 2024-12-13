afff17a251b626dd9644fcf678eb9c5312369657
diosescalera <al217108@edu.uaa.mx>
2024-12-12 19:19:05 -0600

    add gestionar-inventario component with template, styles, and tests; update routes and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { NuevaSolicitudComponent } from "./nueva-solicitud/nueva-solicitud.component";
 import { CargarHorarioComponent } from "./cargar-horario/cargar-horario.component";
+import { GestionarInventarioComponent } from "./gestionar-inventario/gestionar-inventario.component";
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'nueva-solicitud', component: NuevaSolicitudComponent },
     { path: 'cargar-horario', component: CargarHorarioComponent },
+    { path: 'gestionar-inventario', component: GestionarInventarioComponent },
 ];
---
+++ src/app/gestionar-inventario/gestionar-inventario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestionar-inventario',
+  imports: [],
+  templateUrl: './gestionar-inventario.component.html',
+  styleUrl: './gestionar-inventario.component.css'
+})
+export class GestionarInventarioComponent {
+
+}
