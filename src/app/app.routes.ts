7d92c2a7534575cc309556ecff7256c1031a2291
diosescalera <al217108@edu.uaa.mx>
2024-12-13 20:15:49 -0600

    add nuevo-material component with routing and navbar integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { MisSolicitudesComponent } from "./mis-solicitudes/mis-solicitudes.compo
 import { SolicitudesPendientesComponent } from "./solicitudes-pendientes/solicitudes-pendientes.component";
 import { SolicitudesAprobadasComponent } from "./solicitudes-aprobadas/solicitudes-aprobadas.component";
 import { CargarHorarioComponent } from "./cargar-horario/cargar-horario.component";
+import { NuevoMaterialComponent } from "./nuevo-material/nuevo-material.component";
 import { GestionarInventarioComponent } from "./gestionar-inventario/gestionar-inventario.component";
 import { DetallesLaboratorioComponent } from "./detalles-laboratorio/detalles-laboratorio.component";
 
@@ -15,6 +16,7 @@ export const routes: Routes = [
     { path: 'solicitudes-pendientes', component: SolicitudesPendientesComponent },
     { path: 'solicitudes-aprobadas', component: SolicitudesAprobadasComponent },
     { path: 'cargar-horario', component: CargarHorarioComponent },
+    { path: 'nuevo-material', component: NuevoMaterialComponent },
     { path: 'gestionar-inventario', component: GestionarInventarioComponent },
     { path: 'detalles-laboratorio', component: DetallesLaboratorioComponent },
 ];
---
+++ src/app/nuevo-material/nuevo-material.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-material',
+  imports: [],
+  templateUrl: './nuevo-material.component.html',
+  styleUrl: './nuevo-material.component.css'
+})
+export class NuevoMaterialComponent {
+
+}
