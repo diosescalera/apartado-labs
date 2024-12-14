8e9da88f0b4fcfa4b68697e0b830c0272970ed08
diosescalera <al217108@edu.uaa.mx>
2024-12-13 19:48:53 -0600

    add solicitudes-pendientes component with routing and navbar integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { NuevaSolicitudComponent } from "./nueva-solicitud/nueva-solicitud.component";
 import { MisSolicitudesComponent } from "./mis-solicitudes/mis-solicitudes.component";
+import { SolicitudesPendientesComponent } from "./solicitudes-pendientes/solicitudes-pendientes.component";
 import { CargarHorarioComponent } from "./cargar-horario/cargar-horario.component";
 import { GestionarInventarioComponent } from "./gestionar-inventario/gestionar-inventario.component";
 import { DetallesLaboratorioComponent } from "./detalles-laboratorio/detalles-laboratorio.component";
@@ -10,6 +11,7 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'nueva-solicitud', component: NuevaSolicitudComponent },
     { path: 'mis-solicitudes', component: MisSolicitudesComponent },
+    { path: 'solicitudes-pendientes', component: SolicitudesPendientesComponent },
     { path: 'cargar-horario', component: CargarHorarioComponent },
     { path: 'gestionar-inventario', component: GestionarInventarioComponent },
     { path: 'detalles-laboratorio', component: DetallesLaboratorioComponent },
---
+++ src/app/solicitudes-pendientes/solicitudes-pendientes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitudes-pendientes',
+  imports: [],
+  templateUrl: './solicitudes-pendientes.component.html',
+  styleUrl: './solicitudes-pendientes.component.css'
+})
+export class SolicitudesPendientesComponent {
+
+}
