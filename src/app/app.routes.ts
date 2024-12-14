f6badc8e67d66ede18aef02e00b05645f1c7339c
diosescalera <al217108@edu.uaa.mx>
2024-12-13 19:23:54 -0600

    add mis-solicitudes component with template, styles, and tests; update routes and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { NuevaSolicitudComponent } from "./nueva-solicitud/nueva-solicitud.component";
+import { MisSolicitudesComponent } from "./mis-solicitudes/mis-solicitudes.component";
 import { CargarHorarioComponent } from "./cargar-horario/cargar-horario.component";
 import { GestionarInventarioComponent } from "./gestionar-inventario/gestionar-inventario.component";
 import { DetallesLaboratorioComponent } from "./detalles-laboratorio/detalles-laboratorio.component";
@@ -8,6 +9,7 @@ import { DetallesLaboratorioComponent } from "./detalles-laboratorio/detalles-la
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'nueva-solicitud', component: NuevaSolicitudComponent },
+    { path: 'mis-solicitudes', component: MisSolicitudesComponent },
     { path: 'cargar-horario', component: CargarHorarioComponent },
     { path: 'gestionar-inventario', component: GestionarInventarioComponent },
     { path: 'detalles-laboratorio', component: DetallesLaboratorioComponent },
